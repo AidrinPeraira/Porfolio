20639138a14aaa3b9d27ee543ad7f06f386830bd
Aidrin Peraira <aidrin.peraira@gmail.com>
2024-10-30 17:46:00 +0530

    small changes

--- js/form-script.js
+++ js/form-script.js
@@ -5,13 +5,16 @@ function validateForm() {
     const subject = $("input[name='subject']").val();
     const message = $("textarea[name='message']").val();
 
-    // Validate the constants
+    // Validate the constants. If not null the check is done. return for each check one by one.
     if (!name) {
         alert("Name is required.");
         return false;
     }
 
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    //this is a  regular expression
+    // it is used to define the format of strings. Need to study!!
+
     if (!email || !emailPattern.test(email)) {
         alert("Please enter a valid email address.");
         return false;
@@ -26,20 +29,21 @@ function validateForm() {
         alert("Message is required.");
         return false;
     }
-    
+
     return true;
 }
 
 $("#submit-form").submit((e) => {
-    e.preventDefault(); // Prevent default form submission
+    e.preventDefault(); 
 
-    // Validate the form
+    
     if (validateForm()) {
-        // Proceed with the AJAX Code
+        
+        //using ajax code only if th e valiatiion is true for our conditions
         $.ajax({
             url: "https://script.google.com/macros/s/AKfycbxSjZHJ3s0OwzfmFZODSScwQ1c1oV9BPoUP-vqJt868KFhc3ew9qyfHAkDtxZuXfS9dsw/exec",
-            data: $("#submit-form").serialize(), // Serialize form data
-            method: "POST", // Use POST method
+            data: $("#submit-form").serialize(), 
+            method: "POST", 
             success: function (response) {
                 alert("Form submitted successfully!");
                 window.location.reload(); // reload the page on the same spot
